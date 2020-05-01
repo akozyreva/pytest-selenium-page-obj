08e28814c8c95fa2f6b44f1799dd356e4107bd26
akozyreva <anna.ko4523546@gmail.com>
2020-05-01 15:19:56 +0200

    rewrote test_main_pgae test in page-object pattern

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,6 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-    def go_to_login_page(self, browser):
+    def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
