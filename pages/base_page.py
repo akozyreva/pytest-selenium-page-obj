186c9be2611467721d3960bbffa22c5fd68ecd07
akozyreva <anna.ko4523546@gmail.com>
2020-05-01 15:30:44 +0200

    wrote abstract method to catch standard selenium errors

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
 # all helped methods to work with driver
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    # if there is standard selenium exception
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -8,4 +8,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
