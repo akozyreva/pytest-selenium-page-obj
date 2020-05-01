71f8ccc12dc281022c14841c114ac6c698aaa354
akozyreva <anna.ko4523546@gmail.com>
2020-05-01 16:10:31 +0200

    add login page with checks

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        cur_url = self.browser.current_url
+        is_login_url = is_login_url.replace("/", " ").strip()
+        assert is_login_url.split()[-1] == "login", f"It's not a login page, "\
+            "correct url is {cur_url}"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), \
+            "Login form is not presented"
